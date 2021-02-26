2be84f51f2a6eee05c4324a915ac9e98f73554df
kennyfmx <fmxhighlife@gmail.com>
2021-02-26 15:55:28 +0300

    master

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-  if (n > 0) {
+  if (n >= 0) {
       return Number(n.toString().split('').reverse().join(''));
   } else {
       return Number((n * -1).toString().split('').reverse().join(''));
